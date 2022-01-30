d4ad3f17c8fce17e31e24771ce3f9b68f331d7bc
Ahimsa Ashar <ahimsa.ashar.87@gmail.com>
2022-01-30 13:30:50 +0700

    add handler onYearChange and onSearchClick to handle the API Call

--- src/components/movie-list/index.js
+++ src/components/movie-list/index.js
@@ -1,22 +1,51 @@
-import React  from "react";
+import React, { Component } from "react";
 import "./index.css";
 
-function MovieList() {
+export default class MovieList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedYear: null,
+      result: []
+    };
+  }
 
-  return (
-    <div className="layout-column align-items-center mt-50">
-      <section className="layout-row align-items-center justify-content-center">
-        <input type="number" className="large" placeholder="Enter Year eg 2015" data-testid="app-input"/>
-        <button className="" data-testid="submit-button">Search</button>
-      </section>
+  onYearChange = (e) => {
+    this.setState({selectedYear: e.target.value});
+  }
 
-      <ul className="mt-50 styled" data-testid="movieList">
-        <li className="slide-up-fade-in py-10"></li>
-      </ul>
+  onSearchClick = () => {
+    if (this.state.selectedYear) {
+      fetch(`https://jsonmock.hackerrank.com/api/movies?Year=${this.state.selectedYear}`)
+        .then((response) => response.json())
+        .then(result => this.setState({
+          result: result.data
+        }))
+    }
+  }
 
-      <div className="mt-50 slide-up-fade-in" data-testid="no-result"></div>
-    </div>
-  );
+  render() {
+    return (
+      <div className="layout-column align-items-center mt-50">
+        <section className="layout-row align-items-center justify-content-center">
+          <input type="number" className="large" placeholder="Enter Year eg 2015" data-testid="app-input" onBlur={this.onYearChange}/>
+          <button className="" data-testid="submit-button" onClick={this.onSearchClick}>Search</button>
+        </section>
+
+        {this.state.selectedYear && this.state.result.length > 0 && 
+          <ul className="mt-50 styled" data-testid="movieList">
+            {this.state.result.map((resp, index) => {
+              return (
+                <li className="slide-up-fade-in py-10" key={index + 1}>{resp.Title}</li>
+              );
+            })}
+          </ul>
+        }
+        
+        {this.state.selectedYear && this.state.result.length === 0 && 
+          <div className="mt-50 slide-up-fade-in" data-testid="no-result">No Movies found</div>
+        }
+      </div>
+    );
+  }
 }
-
-export default MovieList
